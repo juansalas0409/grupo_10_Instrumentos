9545ce5ea90f7c26e4743feb46aca5fddfc9ef6e
juansalas0409 <juan.salas0409@gmail.com>
2021-10-11 21:54:52 -0300

    prueba

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -12,7 +12,7 @@ const mainController = {
     return res.render("./users/login");
   },
   register: (req, res) => {
-    return res.render("./users/register");
+    return res.render("./users/registro");
   },
 };
 
