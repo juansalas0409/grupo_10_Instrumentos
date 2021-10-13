51d46c9661cddeb434837892c63adde88759f382
juansalas0409 <juan.salas0409@gmail.com>
2021-10-12 23:01:06 -0300

    bugFix

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path')
 const routes = require('./routes/mainRoutes');
 
+app.set('public', path.join(__dirname, 'public'))
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
