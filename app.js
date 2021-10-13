10736a4b4ccc00b77f0fcd46a657687af6d5d7c3
juansalas0409 <juan.salas0409@gmail.com>
2021-10-12 23:13:32 -0300

    Heroku

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const app = express();
 const path = require('path')
 const routes = require('./routes/mainRoutes');
 
+app.use(express.static('./public'))
+
 app.set('public', path.join(__dirname, 'public'))
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, './public')));
 app.use('/', routes);
 
 app.listen(process.env.PORT || 3000, function(){console.log("Se esta corriendo el servidor en http://localhost:3000")});
