2c2bf0712a99b9c267e013c2a5bcb04e9e4fb7f5
Marco Checa <checa.marco2002@gmail.com>
2021-12-09 22:11:44 -0300

    bug fix

--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -7,15 +7,19 @@ const usersFilPath = path.join(__dirname, "../data/usersDB.json");
 const users = JSON.parse(fs.readFileSync(usersFilPath, "utf-8"));
 
 function adminMiddleware(req, res, next) {
-    let user = req.session.userLogged.categoria
-
-    console.log(user)
+    
+if (req.session.userLogged){
+   let user = req.session.userLogged.categoria;
+    
 
   if (user == "admin") {
       next();
     }else{
         res.status(401).json('Debes ser admin')
     }
+}else{
+    res.status(401).json('Debes ser admin')
+}
 }
 
 module.exports = adminMiddleware;
